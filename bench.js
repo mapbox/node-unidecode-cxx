f51a96eac4b80e5807f568fa329d601845cfe70a
Dane Springmeyer <dane@mapbox.com>
2017-01-27 16:40:56 -0800

    update bench to used locally catched file + test both decoders

--- bench.js
+++ bench.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var unidecode = require('unidecode');
 var unidecodeCxx = require('./unidecode');
+var fs = require('fs');
 
 var iterations = 100;
+
+var Decoder = new unidecodeCxx.Decoder();
 
 function compare(data) {
     lines = data.split("\n");
@@ -15,6 +18,7 @@ function compare(data) {
     }
     console.timeEnd("unidecode");
 
+
     console.time("cxx decodable");
     for (var i = 0; i < iterations; i++) {
         for (var j = 0; j < lines.length; j++) {
@@ -23,6 +27,7 @@ function compare(data) {
     }
     console.timeEnd("cxx decodable");
 
+
     console.time("cxx decode");
     for (var i = 0; i < iterations; i++) {
         for (var j = 0; j < lines.length; j++) {
@@ -30,16 +35,28 @@ function compare(data) {
         }
     }
     console.timeEnd("cxx decode");
+
+    console.time("cxx decode2");
+    for (var i = 0; i < iterations; i++) {
+        for (var j = 0; j < lines.length; j++) {
+            Decoder.decode(lines[j]);
+        }
+    }
+    console.timeEnd("cxx decode2");
 }
 
-console.log("Retrieving file...");
-http.get("http://mapbox.s3.amazonaws.com/apendleton/34013-0.txt", function(response) {
-    var body = '';
-    response.on('data', function (chunk) {
-        body += chunk;
+if (fs.existsSync('34013-0.txt')) {
+    compare(fs.readFileSync('34013-0.txt').toString());
+} else {
+    console.log("Retrieving file...");
+    http.get("http://mapbox.s3.amazonaws.com/apendleton/34013-0.txt", function(response) {
+        var body = '';
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+        response.on('end', function () {
+            console.log("Done.")
+            compare(body);
+        });
     });
-    response.on('end', function () {
-        console.log("Done.")
-        compare(body);
-    });
-});
+}
